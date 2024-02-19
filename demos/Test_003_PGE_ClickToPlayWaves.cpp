cc5dc1bb39f105077bed38d6b5cc73394e1b84d8
Bruno Cabral <cabral.brc@gmail.com>
2024-02-18 20:19:57 -0800

    formatting

--- demos/Test_003_PGE_ClickToPlayWaves.cpp
+++ demos/Test_003_PGE_ClickToPlayWaves.cpp
@@ -4,53 +4,48 @@
 #define OLC_SOUNDWAVE
 #include "../olcSoundWaveEngine.h"
 
-
 class Example003 : public olc::PixelGameEngine
 {
 public:
-	Example003()
-	{
-		sAppName = "Testing Sample Rates";
-	}
+    Example003()
+    {
+        sAppName = "Testing Sample Rates";
+    }
 
 protected: // Sound Specific
-	olc::sound::WaveEngine engine;
+    olc::sound::WaveEngine engine;
 
-	olc::sound::Wave sample_44100;
-	olc::sound::Wave sample_48000;
+    olc::sound::Wave sample_44100;
+    olc::sound::Wave sample_48000;
 
-	
 public:
-	bool OnUserCreate() override
-	{
-		sample_44100.LoadAudioWaveform("./assets/Sample_Test_44100.wav");
-		sample_48000.LoadAudioWaveform("./assets/Sample_Test_48000.wav");		
-		engine.InitialiseAudio(44100,2);		
-		return true;
-	}
+    bool OnUserCreate() override
+    {
+        sample_44100.LoadAudioWaveform("./assets/Sample_Test_44100.wav");
+        sample_48000.LoadAudioWaveform("./assets/Sample_Test_48000.wav");
+        engine.InitialiseAudio(44100, 2);
+        return true;
+    }
 
-	bool OnUserUpdate(float fElapsedTime) override
-	{		
-		if (GetKey(olc::Key::K1).bPressed)
-			engine.PlayWaveform(&sample_44100);
-		
-		if (GetKey(olc::Key::K2).bPressed)
-			engine.PlayWaveform(&sample_48000);
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+        if (GetKey(olc::Key::K1).bPressed)
+            engine.PlayWaveform(&sample_44100);
 
+        if (GetKey(olc::Key::K2).bPressed)
+            engine.PlayWaveform(&sample_48000);
 
-		Clear(olc::BLACK);
-		DrawString({ 4,  4 }, "[1] Play 44100Hz x 16-bit Sample");
-		DrawString({ 4, 14 }, "[2] Play 48000Hz x 24-bit Sample");		
-		return true;
-	}
+        Clear(olc::BLACK);
+        DrawString({4, 4}, "[1] Play 44100Hz x 16-bit Sample");
+        DrawString({4, 14}, "[2] Play 48000Hz x 24-bit Sample");
+        return true;
+    }
 };
-
-
 
 int main()
 {
-	Example003 demo;
-	if (demo.Construct(640, 480, 2, 2))
-		demo.Start();
-	return 0;
+    Example003 demo;
+    if (demo.Construct(640, 480, 2, 2))
+        demo.Start();
+    return 0;
 }
--- demos/Test_004_PGE_RawDataPlayback.cpp
+++ demos/Test_004_PGE_RawDataPlayback.cpp
@@ -4,54 +4,51 @@
 #define OLC_SOUNDWAVE
 #include "../olcSoundWaveEngine.h"
 
-
 class Example004 : public olc::PixelGameEngine
 {
 public:
-	Example004()
-	{
-		sAppName = "Testing Custom Wave Gen";
-	}
+    Example004()
+    {
+        sAppName = "Testing Custom Wave Gen";
+    }
 
 protected: // Sound Specific
-	olc::sound::WaveEngine engine;
+    olc::sound::WaveEngine engine;
 
-	olc::sound::Wave custom_wave;
+    olc::sound::Wave custom_wave;
 
 public:
-	bool OnUserCreate() override
-	{
-		engine.InitialiseAudio(44100, 1);
+    bool OnUserCreate() override
+    {
+        engine.InitialiseAudio(44100, 1);
 
-		custom_wave = olc::sound::Wave(1, sizeof(uint8_t), 44100, 44100);
+        custom_wave = olc::sound::Wave(1, sizeof(uint8_t), 44100, 44100);
 
-		for (size_t i = 0; i < 44100; i++)
-		{
-			double dt = 1.0 / 44100.0;
-			double t = double(i) * dt;
-			custom_wave.file.data()[i] = float(0.5 * sin(2.0 * 440.0 * 3.14159 * t));
-		}
+        for (size_t i = 0; i < 44100; i++)
+        {
+            double dt = 1.0 / 44100.0;
+            double t = double(i) * dt;
+            custom_wave.file.data()[i] = float(0.5 * sin(2.0 * 440.0 * 3.14159 * t));
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		if (GetKey(olc::Key::K1).bPressed)
-			engine.PlayWaveform(&custom_wave);
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+        if (GetKey(olc::Key::K1).bPressed)
+            engine.PlayWaveform(&custom_wave);
 
-		Clear(olc::BLACK);
-		DrawString({ 4,  4 }, "[1] Play 44100Hz x 8-bit Custom Sample");		
-		return true;
-	}
+        Clear(olc::BLACK);
+        DrawString({4, 4}, "[1] Play 44100Hz x 8-bit Custom Sample");
+        return true;
+    }
 };
-
-
 
 int main()
 {
-	Example004 demo;
-	if (demo.Construct(640, 480, 2, 2))
-		demo.Start();
-	return 0;
+    Example004 demo;
+    if (demo.Construct(640, 480, 2, 2))
+        demo.Start();
+    return 0;
 }
